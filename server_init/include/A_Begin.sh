ec4cdef132519369d6959496bf1fae3ad43779c0
Allen <ydcode@users.noreply.github.com>
2017-12-05 18:27:20 +0800

    Update A_Begin.sh

--- server_init/include/A_Begin.sh
+++ server_init/include/A_Begin.sh
@@ -49,10 +49,10 @@ Echo_Yellow "Server: ${ServerEnv}"
 
 
 JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz"
-MAVEN_URL="http://www-eu.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz"
+MAVEN_URL="http://apache.mirror.colo-serv.net/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz"
 JDK_FILE="jdk-8u131-linux-x64.tar.gz"
 JDK_NAME="jdk1.8.0_131"
-MAVEN_NAME="apache-maven-3.5.0"
+MAVEN_NAME="apache-maven-3.5.2"
 Nginx_Ver="1.12.0"
 
 
