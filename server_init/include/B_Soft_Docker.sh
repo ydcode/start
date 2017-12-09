4d20c36c432582e7c40d63a8bb87250f420961d3
Allen <ydcode@users.noreply.github.com>
2017-12-09 16:10:21 +0800

    Update B_Soft_Docker.sh

--- server_init/include/B_Soft_Docker.sh
+++ server_init/include/B_Soft_Docker.sh
@@ -1,7 +1,7 @@
-sudo yum remove docker docker-common  docker-selinux docker-engine
+sudo yum remove -y docker docker-common  docker-selinux docker-engine
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
-sudo yum install docker-ce
+sudo yum install -y  docker-ce
 sudo systemctl start docker
 
 
