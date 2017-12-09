fc295e3922a783641025a766f040cc30a8f04d04
Allen <ydcode@users.noreply.github.com>
2017-12-09 16:05:12 +0800

    Update B_Soft_Docker.sh

--- server_init/include/B_Soft_Docker.sh
+++ server_init/include/B_Soft_Docker.sh
@@ -3,3 +3,18 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
 sudo yum install docker-ce
 sudo systemctl start docker
+
+
+if grep -q "/usr/local/bin" "/etc/profile"; then #条目已存在
+        Echo_Green "/usr/local/bin Exist."
+else
+    echo 'export PATH=$PATH:/usr/local/bin' >>  /etc/profile
+fi
+source /etc/profile
+source /etc/profile
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+
