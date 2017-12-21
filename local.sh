6414379c4b1cc88d75c49f878c4ea9389e19f933
Allen <ydcode@users.noreply.github.com>
2017-12-21 21:45:33 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -4,26 +4,7 @@ add-apt-repository ppa:hzwhuang/ss-qt5
 apt-get update
 apt-get install shadowsocks-qt5
 
-/usr/bin/guake &
-/usr/bin/ss-qt5 &
-
-if grep -q "nohup /usr/bin/guake &" "$HOME/.bashrc"; then #条目已存在
-  Echo_Green "$HOME/.bashrc  nohup /usr/bin/guake --- auto start Exist."
-else
-  echo "nohup /usr/bin/guake &" >> $HOME/.bashrc
-  source $HOME/.bashrc
-fi
-    
-    
-if grep -q "nohup /usr/bin/ss-qt5 &" "$HOME/.bashrc"; then #条目已存在
-  Echo_Green "nohup /usr/bin/ss-qt5 &  --- auto start Exist."
-else
-  echo "nohup /usr/bin/ss-qt5 &" >> $HOME/.bashrc
-  source $HOME/.bashrc
-fi
-    
-
-    
+# ubuntu auto start guake ss-qt5    
 # sougou https://jingyan.baidu.com/article/a3aad71aa1abe7b1fa009641.html
 # 云音乐
 # chrome
