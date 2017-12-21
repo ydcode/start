cf59d62fb908ce8d809e51e69e464d4341a27d4e
Allen <ydcode@users.noreply.github.com>
2017-12-21 22:03:38 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -4,6 +4,26 @@ add-apt-repository ppa:hzwhuang/ss-qt5
 apt-get update
 apt-get install shadowsocks-qt5
 
+
+sudo apt-get remove docker docker-engine docker.io
+
+sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+ "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+ $(lsb_release -cs) \
+ stable"
+sudo apt-get update 
+   
+ sudo apt-get install docker-ce
+ 
+ 
 # ubuntu auto start guake ss-qt5    
 # sougou https://jingyan.baidu.com/article/a3aad71aa1abe7b1fa009641.html
 # 云音乐
