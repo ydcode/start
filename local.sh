f259d70221553c35f6c5813c2387e46436df1bb3
Allen <ydcode@users.noreply.github.com>
2017-12-21 21:07:53 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -4,7 +4,13 @@ add-apt-repository ppa:hzwhuang/ss-qt5
 apt-get update
 apt-get install shadowsocks-qt5
 
-
+if grep -q "/usr/bin/guake" "/etc/profile"; then #条目已存在
+  Echo_Green "/etc/profile  guake auto start Exist."
+else
+  echo "/usr/bin/guake &" >> /etc/profile
+  source /etc/profile
+fi
+    
 # sougou https://jingyan.baidu.com/article/a3aad71aa1abe7b1fa009641.html
 # 云音乐
 
