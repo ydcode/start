e410e44d39e475908133b339cd5d0672ba67ad23
Allen <ydcode@users.noreply.github.com>
2017-12-21 21:02:15 +0800

    Create local.sh

---
+++ local.sh
@@ -0,0 +1,16 @@
+apt install -y gdebi shutter guake
+apt upgrade -y
+add-apt-repository ppa:hzwhuang/ss-qt5
+apt-get update
+apt-get install shadowsocks-qt5
+
+
+# sougou https://jingyan.baidu.com/article/a3aad71aa1abe7b1fa009641.html
+# 云音乐
+
+
+wps
+jdk
+git
+maven
+chrome
