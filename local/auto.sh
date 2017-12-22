c4c5687339837b22be0661d33e3c948bf76f4fcc
Allen <ydcode@users.noreply.github.com>
2017-12-23 02:58:02 +0800

    Update auto.sh

--- local/auto.sh
+++ local/auto.sh
@@ -1,49 +1,44 @@
 #!/bin/bash
 
 
+JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz"
+JDK_FILE="jdk-8u131-linux-x64.tar.gz"
+JDK_NAME="jdk1.8.0_131"
+
+
 Install_JDK()
 {
-    files=`ls /usr/java/`
-    if [ -z "$files" ]; then
-        rm -rf /usr/java
-    fi
-    cd ${CurrentDir}
-    pwd
-    if [ ! -d "/usr/java/" ]; then
-    	source /etc/profile
-        if [ ! -e "${JDK_FILE}" ]; then
-            wget --continue --no-check-certificate --header "Cookie: oraclelicense=a" ${JDK_URL}
-#            'http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz'
-        fi
-        mkdir /usr/java && tar zxvf jdk*.tar.gz -C /usr/java/
-        ln -s /usr/java/${JDK_NAME}/bin/java /usr/local/bin/java #必要
+	files=`ls /usr/java/`
+	if [ -z "$files" ]; then
+		rm -rf /usr/java
+	fi
+		cd $HOME
+		pwd
+	if [ ! -d "/usr/java/" ]; then
+		source /etc/profile
+		if [ ! -e "${JDK_FILE}" ]; then
+		    wget --continue --no-check-certificate --header "Cookie: oraclelicense=a" ${JDK_URL}
+	#            'http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz'
+		fi
+		mkdir /usr/java && tar zxvf jdk*.tar.gz -C /usr/java/
+		ln -s /usr/java/${JDK_NAME}/bin/java /usr/local/bin/java #必要
 
-        if grep -q "/usr/java/jdk" "/etc/profile"; then #条目已存在
-            Echo_Green "JDK Item Exist."
-        else
-            echo "export JAVA_HOME=/usr/java/${JDK_NAME}" >> /etc/profile
-            echo "export JRE_HOME=/usr/java/${JDK_NAME}/jre" >> /etc/profile
-            echo "export MAVEN_HOME=/usr/maven" >> /etc/profile
-            echo 'export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin:$JRE_HOME/include:$MAVEN_HOME/bin:/usr/local/bin' >>  /etc/profile
-            source /etc/profile
+		if grep -q "/usr/java/jdk" "/etc/profile"; then #条目已存在
+		    echo "JDK Item Exist."
+		else
+		    echo "export JAVA_HOME=/usr/java/${JDK_NAME}" >> /etc/profile
+		    echo "export JRE_HOME=/usr/java/${JDK_NAME}/jre" >> /etc/profile
+		    echo "export MAVEN_HOME=/usr/maven" >> /etc/profile
+		    echo 'export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin:$JRE_HOME/include:$MAVEN_HOME/bin:/usr/local/bin' >>  /etc/profile
+		    source /etc/profile
 
-            Echo_Yellow "Open new Terminal and once again './main.sh' and "
-            Echo_Green "Ctrl + C to exit"
-            get_char
-        fi
+		    echo "Open new Terminal and once again './main.sh' and "
+		fi
 
-        cd ${CurrentDir}
+		cd $HOME
 	else
-		Echo_Green "JDK already installed."
-    fi
+		echo "JDK already installed."
+	fi
 }
 
-Install_MVN(){
-        if [ ! -d "/usr/maven/" ]; then
-            cd ${CurrentDir}
-            wget ${MAVEN_URL}
-	    mkdir /usr/maven && tar xzvf apache-maven*.tar.gz --strip-components 1  -C /usr/maven/
-        fi
-}
-
-
+Install_JDK
