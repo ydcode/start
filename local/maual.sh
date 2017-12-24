8ec011caa18d65fce7f86debd70dd20f724508f8
Allen <ydcode@users.noreply.github.com>
2017-12-24 23:28:47 +0800

    Update maual.sh

--- local/maual.sh
+++ local/maual.sh
@@ -54,9 +54,13 @@ Comment=guake
 #download docker deb
 sudo gdebi docker.deb
 # shutter guake
-idea without jdk   ---- sudo idea by normal user
+
+#idea without jdk   ---- sudo idea by normal user
 使用前请将“0.0.0.0 account.jetbrains.com”添加到hosts文件中
 安装 plugin: lombok gitignore jrebel + 激活码即可
+Shift+Ctrl+Alt+/ --- 选择Registry --- compiler.automake.allow.when.app.running enable
+setting --- comipile --- Build Project Automatically
+restart idea
 
 init repo
 apt upgrade -y
