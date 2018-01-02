6b2820f4814cf7bbd8d504ab733b49aad1bc8c06
Allen <ydcode@users.noreply.github.com>
2018-01-02 21:24:00 +0800

    Update local_ubuntu_auto.sh

--- local/local_ubuntu_auto.sh
+++ local/local_ubuntu_auto.sh
@@ -27,7 +27,7 @@ Install_JDK()
 		    echo "JDK Item Exist."
 		else
 	
-			echo 'export PATH=$HOME_U/bin:$HOME_U/.local/bin:$PATH' >> ~/.bashrc
+			#echo 'export PATH=$HOME_U/bin:$HOME_U/.local/bin:$PATH' >> ~/.bashrc
 			echo 'export PATH=$PATH:/mnt/c/Program\ Files/Docker/Docker/resources/bin' >> ~/.bashrc
 			echo "export DOCKER_HOST='tcp://0.0.0.0:2375'" >> ~/.bashrc
 			source ~/.bashrc
