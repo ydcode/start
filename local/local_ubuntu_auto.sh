e7e0e5c3018883a91f28c96496522448a980cee2
Allen <ydcode@users.noreply.github.com>
2018-01-02 21:06:49 +0800

    Update local_ubuntu_auto.sh

--- local/local_ubuntu_auto.sh
+++ local/local_ubuntu_auto.sh
@@ -26,12 +26,12 @@ Install_JDK()
 		if grep -q "/usr/java/jdk" "~/.bashrc"; then #条目已存在
 		    echo "JDK Item Exist."
 		else
-			echo 'export JAVA_HOME=/usr/java/${JDK_NAME}' >> ~/.bashrc
-			echo 'export JRE_HOME=/usr/java/${JDK_NAME}/jre' >> ~/.bashrc
-			echo 'export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin:$JRE_HOME/include:/usr/maven/bin:/usr/local/bin' >>  ~/.bashrc
+			echo 'export JAVA_HOME_U=/usr/java/${JDK_NAME}' >> ~/.bashrc
+			echo 'export JRE_HOME_U=/usr/java/${JDK_NAME}/jre' >> ~/.bashrc
+			echo 'export PATH=$PATH:$JAVA_HOME_U/bin:$JRE_HOME_U/bin:$JRE_HOME_U/include:/usr/maven/bin:/usr/local/bin' >>  ~/.bashrc
 			source ~/.bashrc
 
-			echo 'export PATH=$HOME/bin:$HOME/.local/bin:$PATH' >> ~/.bashrc
+			echo 'export PATH=$HOME_U/bin:$HOME_U/.local/bin:$PATH' >> ~/.bashrc
 			echo 'export PATH=$PATH:/mnt/c/Program\ Files/Docker/Docker/resources/bin' >> ~/.bashrc
 			echo "export DOCKER_HOST='tcp://0.0.0.0:2375'" >> ~/.bashrc
 			source ~/.bashrc
