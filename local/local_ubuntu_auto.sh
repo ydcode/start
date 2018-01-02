3b0aa2142b05e3c3bb1e97f50931ab5854cfe2f5
Allen <ydcode@users.noreply.github.com>
2018-01-02 19:29:03 +0800

    Update local_ubuntu_auto.sh

--- local/local_ubuntu_auto.sh
+++ local/local_ubuntu_auto.sh
@@ -1,3 +1,5 @@
+apt install -y proxychains wget curl
+
 
 JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.tar.gz"
 JDK_FILE="jdk-8u152-linux-x64.tar.gz"
