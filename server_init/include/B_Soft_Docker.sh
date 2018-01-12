4aa21c2ee6d5508b4baf466379d5aa817dd04d58
Allen <ydcode@users.noreply.github.com>
2018-01-12 19:18:22 +0800

    Update B_Soft_Docker.sh

--- server_init/include/B_Soft_Docker.sh
+++ server_init/include/B_Soft_Docker.sh
@@ -5,13 +5,12 @@ sudo yum install -y  docker-ce
 sudo systemctl start docker
 
 
-if grep -q "/usr/local/bin" "/etc/profile"; then #条目已存在
+if grep -q "/usr/local/bin" "~/.bashrc"; then #条目已存在
         Echo_Green "/usr/local/bin Exist."
 else
-    echo 'export PATH=$PATH:/usr/local/bin' >>  /etc/profile
+    echo 'export PATH=$PATH:/usr/local/bin' >>  ~/.bashrc
 fi
-source /etc/profile
-source /etc/profile
+source ~/.bashrc
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
