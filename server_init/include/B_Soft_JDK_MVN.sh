ece997a1d18861c30ece96177030439a8cc2beb4
Allen <ydcode@users.noreply.github.com>
2018-01-12 19:16:54 +0800

    Update B_Soft_JDK_MVN.sh

--- server_init/include/B_Soft_JDK_MVN.sh
+++ server_init/include/B_Soft_JDK_MVN.sh
@@ -10,7 +10,7 @@ Install_JDK()
     cd ${CurrentDir}
     pwd
     if [ ! -d "/usr/java/" ]; then
-    	source /etc/profile
+    	source ~/.bashrc
         if [ ! -e "${JDK_FILE}" ]; then
             wget --continue --no-check-certificate --header "Cookie: oraclelicense=a" ${JDK_URL}
 #            'http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz'
@@ -18,14 +18,14 @@ Install_JDK()
         mkdir /usr/java && tar zxvf jdk*.tar.gz -C /usr/java/
         ln -s /usr/java/${JDK_NAME}/bin/java /usr/local/bin/java #必要
 
-        if grep -q "/usr/java/jdk" "/etc/profile"; then #条目已存在
+        if grep -q "/usr/java/jdk" "~/.bashrc"; then #条目已存在
             Echo_Green "JDK Item Exist."
         else
-            echo "export JAVA_HOME=/usr/java/${JDK_NAME}" >> /etc/profile
-            echo "export JRE_HOME=/usr/java/${JDK_NAME}/jre" >> /etc/profile
-            echo "export MAVEN_HOME=/usr/maven" >> /etc/profile
-            echo 'export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin:$JRE_HOME/include:$MAVEN_HOME/bin:/usr/local/bin' >>  /etc/profile
-            source /etc/profile
+            echo "export JAVA_HOME=/usr/java/${JDK_NAME}" >> ~/.bashrc
+            echo "export JRE_HOME=/usr/java/${JDK_NAME}/jre" >> ~/.bashrc
+            echo "export MAVEN_HOME=/usr/maven" >> ~/.bashrc
+            echo 'export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin:$JRE_HOME/include:$MAVEN_HOME/bin:/usr/local/bin' >>  ~/.bashrc
+            source ~/.bashrc
 
             Echo_Yellow "Open new Terminal and once again './main.sh' and "
             Echo_Green "Ctrl + C to exit"
