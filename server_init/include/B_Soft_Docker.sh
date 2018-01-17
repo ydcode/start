3139f968630a94144f534081744abbe3270ff1fe
Allen <ydcode@users.noreply.github.com>
2018-01-17 21:54:45 +0800

    Update B_Soft_Docker.sh

--- server_init/include/B_Soft_Docker.sh
+++ server_init/include/B_Soft_Docker.sh
@@ -1,19 +1,22 @@
-sudo yum remove -y docker docker-common  docker-selinux docker-engine
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
-sudo yum install -y  docker-ce
-sudo systemctl start docker
+if [ ! -e "/usr/bin/docker" ]; then
 
+        sudo yum remove -y docker docker-common  docker-selinux docker-engine
+        sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+        sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
+        sudo yum install -y  docker-ce
+        sudo systemctl start docker
 
-if grep -q "/usr/local/bin" "~/.bashrc"; then #条目已存在
-        Echo_Green "/usr/local/bin Exist."
-else
-    echo 'export PATH=$PATH:/usr/local/bin' >>  ~/.bashrc
+        if grep -q "/usr/local/bin" "~/.bashrc"; then #条目已存在
+                Echo_Green "/usr/local/bin Exist."
+        else
+            echo 'export PATH=$PATH:/usr/local/bin' >>  ~/.bashrc
+        fi
+        source ~/.bashrc
+        sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+        docker-compose --version
 fi
-source ~/.bashrc
-
-sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+    
+    
 
 
