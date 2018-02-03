e7cc7043b5ee7e214664aad6deb72160aed39b7e
Allen <ydcode@users.noreply.github.com>
2018-02-04 00:20:02 +0800

    Update backup.sh

--- tools/backup.sh
+++ tools/backup.sh
@@ -7,3 +7,27 @@ backup_or_restore=$4
 restore_date=$5
 
 set -e
+
+
+function backup_volume {
+  volume_name=$1
+  backup_destination=$2
+  date_suffix=$(date -I)
+
+  docker run --rm -v $volume_name:/data -v $backup_destination:/backup ubuntu tar -zcvf /backup/$volume_name-$date_suffix.tar /data
+}
+
+function restore_volume {
+  volume_name=$1
+  backup_destination=$2
+  date=$3
+
+  docker run --rm -v $volume_name:/data ubuntu find /data -mindepth 1 -delete
+  docker run --rm -v $volume_name:/data -v $backup_destination:/backup ubuntu tar -xvf /backup/$volume_name-$date.tar -C .
+}
+
+echo "Stopping running mysql container"
+docker-compose -f /home/gce/start/docker-compose/share-data -p mysql stop
+
+echo "Perform backup MySQL"
+backup_volume mysql_data /home/backup
