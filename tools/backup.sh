07bd096be43c88302d23be3f1017e93715216a14
Allen <ydcode@users.noreply.github.com>
2018-02-04 00:32:00 +0800

    Update backup.sh

--- tools/backup.sh
+++ tools/backup.sh
@@ -12,7 +12,7 @@ set -e
 function backup_volume {
   volume_name=$1
   backup_destination=$2
-  date_suffix=$(date -I)
+  date_suffix=$(date +%Y-%m-%d-%H)
 
   docker run --rm -v $volume_name:/data -v $backup_destination:/backup ubuntu tar -zcvf /backup/$DASHBOARD_DOMAIN-$volume_name-$date_suffix.tar /data
 }
