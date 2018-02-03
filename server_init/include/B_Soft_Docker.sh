9516913053af4d1cecbcf89ad4464714dd53e583
Allen <ydcode@users.noreply.github.com>
2018-02-03 09:41:09 +0800

    Update B_Soft_Docker.sh

--- server_init/include/B_Soft_Docker.sh
+++ server_init/include/B_Soft_Docker.sh
@@ -16,6 +16,8 @@ if [ ! -e "/usr/bin/docker" ]; then
         sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
         docker-compose --version
+        
+        sudo systemctl start docker
 fi
     
     
