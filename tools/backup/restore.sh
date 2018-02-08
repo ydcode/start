d8616a5f88d9f9499c2e47b1f8addf3a3c94e4c8
Allen <ydcode@users.noreply.github.com>
2018-02-08 14:17:48 +0800

    Update restore.sh

--- tools/backup/restore.sh
+++ tools/backup/restore.sh
@@ -14,8 +14,10 @@ function restore_volume {
 }
 
 echo "Stopping running MySQL && RocksDB container"
-docker-compose -f $compose_file_path -p mysql stop
-docker-compose -f $compose_file_path -p rocksdb-server stop
+
+docker-compose -f $compose_file_path down --remove-orphans
+# docker-compose -f $compose_file_path -p mysql stop
+# docker-compose -f $compose_file_path -p rocksdb-server stop
 
 echo "Perform Restore MySQL"
 MySQL_FILE=`ls /home/backup/ | grep mysql`
@@ -25,7 +27,7 @@ echo "Perform Restore RocksDB"
 ROCK_FILE=`ls /home/backup/ | grep rocksdb`
 restore_volume rocksdb_data /home/backup $ROCK_FILE
 
-
+docker-compose -f $compose_file_path up -d
 
 echo "---------------------------------------------------------------------------ls -lh /home/backup/---------------------"
 ls -lh /home/backup/
