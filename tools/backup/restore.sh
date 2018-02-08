4d7245a55a6944f35497e2374ae215f35d09f54f
Allen <ydcode@users.noreply.github.com>
2018-02-08 11:00:33 +0800

    Update restore.sh

--- tools/backup/restore.sh
+++ tools/backup/restore.sh
@@ -7,9 +7,10 @@ set -e
 function restore_volume {
   volume_name=$1
   backup_destination=$2
+  file_name=$3
 
   docker run --rm -v $volume_name:/data ubuntu find /data -mindepth 1 -delete
-  docker run --rm -v $volume_name:/data -v $backup_destination:/backup ubuntu tar -xvf /backup/*$volume_name-*.tar -C .
+  docker run --rm -v $volume_name:/data -v $backup_destination:/backup ubuntu tar -xvf /backup/$file_name -C .
 }
 
 echo "Stopping running MySQL && RocksDB container"
@@ -17,10 +18,10 @@ docker-compose -f $compose_file_path -p mysql stop
 docker-compose -f $compose_file_path -p rocksdb-server stop
 
 echo "Perform Restore MySQL"
-restore_volume mysql_data /home/backup
+restore_volume mysql_data /home/backup us.apipie.com-mysql_data-2018-02-03-16.tar
 
 echo "Perform Restore RocksDB"
-restore_volume rocksdb_data /home/backup
+restore_volume rocksdb_data /home/backup us.apipie.com-rocksdb_data-2018-02-03-16.tar
 
 
 
