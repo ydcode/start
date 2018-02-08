2aa493396e174d3bae07954da65bcf17a0151176
Allen <ydcode@users.noreply.github.com>
2018-02-08 10:08:11 +0800

    Update restore.sh

--- tools/backup/restore.sh
+++ tools/backup/restore.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
 
 compose_file_path=/home/gce/start/docker-compose/share-data/docker-compose.yml
-project_name=${2,,}
-backup_path=$3
-backup_or_restore=$4
-restore_date=$5
 
 set -e
-
-
-function backup_volume {
-  volume_name=$1
-  backup_destination=$2
-  date_suffix=$(date +%Y-%m-%d-%H)
-
-  docker run --rm -v $volume_name:/data -v $backup_destination:/backup ubuntu tar -zcvf /backup/$DASHBOARD_DOMAIN-$volume_name-$date_suffix.tar /data
-}
 
 function restore_volume {
   volume_name=$1
   backup_destination=$2
-  date=$3
 
   docker run --rm -v $volume_name:/data ubuntu find /data -mindepth 1 -delete
-  docker run --rm -v $volume_name:/data -v $backup_destination:/backup ubuntu tar -xvf /backup/$volume_name-$date.tar -C .
+  docker run --rm -v $volume_name:/data -v $backup_destination:/backup ubuntu tar -xvf /backup/*$volume_name-*.tar -C .
 }
 
-echo "Stopping running mysql container"
+echo "Stopping running MySQL && RocksDB container"
 docker-compose -f $compose_file_path -p mysql stop
 docker-compose -f $compose_file_path -p rocksdb-server stop
 
-echo "Perform backup MySQL"
-backup_volume mysql_data /home/backup
+echo "Perform Restore MySQL"
+restore_volume mysql_data /home/backup
 
-echo "Perform backup RocksDB"
-backup_volume rocksdb_data /home/backup
+echo "Perform Restore RocksDB"
+restore_volume rocksdb_data /home/backup
 
 
-echo "---------------------------------------------------------------------------ls -al /home/backup/---------------------"
-ls -al /home/backup/
-echo "--------------------------------------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------ls -lh /home/backup/---------------------"
+ls -lh /home/backup/
+echo "---------------------------------------------------------------------------cd /home/backup/-------------------------"
+
+
+
