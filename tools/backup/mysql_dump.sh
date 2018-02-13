8f1dd96f6133290b85f07c9e0e1fa7d2b39bed79
Allen <ydcode@users.noreply.github.com>
2018-02-13 18:22:40 +0800

    Update mysql_dump.sh

--- tools/backup/mysql_dump.sh
+++ tools/backup/mysql_dump.sh
@@ -5,7 +5,7 @@ set -e
 function mysql_dump {
   backup_destination=$1
   date_suffix=$(date +%Y-%m-%d-%H)
-  docker exec mysql sh -c 'exec mysqldump --databases '${DB_NAME}' -uroot -p"$MYSQL_ROOT_PASSWORD"'  > $backup_destination/$DB_NAME_$date_suffix.sql
+  docker exec mysql sh -c 'exec mysqldump --databases '${DB_NAME}' -uroot -p"$MYSQL_ROOT_PASSWORD"'  > $backup_destination/${DB_NAME}-$date_suffix.sql
 }
 
 echo "Perform Dump MySQL"
