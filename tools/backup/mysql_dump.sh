9b88ab73f94cf5b4502f60119a4fb87ab9f8f9a1
Allen <ydcode@users.noreply.github.com>
2018-02-13 18:17:04 +0800

    Update mysql_dump.sh

--- tools/backup/mysql_dump.sh
+++ tools/backup/mysql_dump.sh
@@ -8,15 +8,9 @@ function mysql_dump {
   docker exec mysql sh -c 'exec mysqldump --databases '${DB_NAME}' -uroot -p"$MYSQL_ROOT_PASSWORD"'  > $backup_destination/$DB_NAME_$date_suffix.sql
 }
 
-
-echo "Stopping running MySQL && RocksDB container"
-docker-compose -f $compose_file_path down --remove-orphans
-
 echo "Perform Dump MySQL"
 mysql_dump /home/backup
 
-echo "Starting MySQL && RocksDB container"
-docker-compose -f $compose_file_path up -d
 echo "---------------------------------------------------------------------------ls -lh /home/backup/---------------------"
 ls -lh /home/backup/
 echo "---------------------------------------------------------------------------cd /home/backup/-------------------------"
