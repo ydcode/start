234367a6e558c81589f79263a8cb5d4b7f96c1be
Allen <ydcode@users.noreply.github.com>
2018-02-13 18:53:17 +0800

    Update mysql_import.sh

--- tools/backup/mysql_import.sh
+++ tools/backup/mysql_import.sh
@@ -1,18 +1,16 @@
-compose_file_path=/home/gce/start/docker-compose/share-data/docker-compose.yml
+SQL_FILE=$1
 
 set -e
 
 function mysql_import {
-  sql_file=$1
   date_suffix=$(date +%Y-%m-%d-%H)
-  docker exec mysql sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD"'  ${DB_NAME} < $sql_file
+  docker exec mysql sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD"'  ${DB_NAME} < $1
 }
 
 
-SQL_FILE=`ls /home/backup/ | grep [.]sql`
 
 echo "Perform Import MySQL"
-mysql_import SQL_FILE
+mysql_import $SQL_FILE
 
 echo "---------------------------------------------------------------------------ls -lh /home/backup/---------------------"
 ls -lh /home/backup/
