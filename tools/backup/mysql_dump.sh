e46d6c319d0b2a4f1350ed1448ca0f21acea0f91
Allen <ydcode@users.noreply.github.com>
2018-02-13 18:14:55 +0800

    Update mysql_dump.sh

--- tools/backup/mysql_dump.sh
+++ tools/backup/mysql_dump.sh
@@ -1,11 +1,11 @@
+compose_file_path=/home/gce/start/docker-compose/share-data/docker-compose.yml
 
 set -e
 
 function mysql_dump {
-  volume_name=$1
-  backup_destination=$2
+  backup_destination=$1
   date_suffix=$(date +%Y-%m-%d-%H)
-  docker exec mysql sh -c 'exec mysqldump --databases '${DB_NAME}' -uroot -p"$MYSQL_ROOT_PASSWORD"'  > $DB_NAME_`date +%Y-%m-%d-%H-%M`.sql.sql
+  docker exec mysql sh -c 'exec mysqldump --databases '${DB_NAME}' -uroot -p"$MYSQL_ROOT_PASSWORD"'  > $backup_destination/$DB_NAME_$date_suffix.sql
 }
 
 
@@ -13,8 +13,9 @@ echo "Stopping running MySQL && RocksDB container"
 docker-compose -f $compose_file_path down --remove-orphans
 
 echo "Perform Dump MySQL"
-mysql_dump mysql_data /home/backup
+mysql_dump /home/backup
 
+echo "Starting MySQL && RocksDB container"
 docker-compose -f $compose_file_path up -d
 echo "---------------------------------------------------------------------------ls -lh /home/backup/---------------------"
 ls -lh /home/backup/
