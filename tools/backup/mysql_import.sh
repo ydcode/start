1ee07c0730943c4bd9829fd250fc8cf874a94260
Allen <ydcode@users.noreply.github.com>
2018-02-14 11:41:39 +0800

    Update mysql_import.sh

--- tools/backup/mysql_import.sh
+++ tools/backup/mysql_import.sh
@@ -2,8 +2,6 @@ SQL_FILE=$1
 
 set -e
 
-
-
 if [ -z "$SQL_FILE" ]; then
 
     echo "usage: mysql_import.sh  ***.sql"
@@ -11,9 +9,7 @@ if [ -z "$SQL_FILE" ]; then
 fi
 echo "Perform Import MySQL"
 
-docker exec mysql sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD"'  ${DB_NAME} < $SQL_FILE
-
-
+docker exec mysql sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" bootdb < $SQL_FILE'
 
 echo "---------------------------------------------------------------------------ls -lh /home/backup/---------------------"
 ls -lh /home/backup/
