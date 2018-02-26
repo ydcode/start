f05c6dc7688a92fb34396c6bca36a5d26bb66cc9
Allen <ydcode@users.noreply.github.com>
2018-02-26 16:42:33 +0800

    Update install.sh

--- server_init/install.sh
+++ server_init/install.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:/usr/java/jdk1.8.0_101/bin:/usr/java/jdk1.8.0_101/jre/bin:/usr/java/jdk1.8.0_101/include:/usr/maven/apache-maven-3.5.0/bin:/root/bin
+export PATH
 
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use root to install lnmp"
+    exit 1
+fi
+. include/A_Begin.sh
+. include/B_Soft_Docker.sh
+. include/B_Soft_JDK_MVN.sh
+
+mkdir /home/backup
+#安装Jemollo
