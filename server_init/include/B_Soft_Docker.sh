ff8cd22e5e14c4cd8cf9ccfa07aceb2a409005a1
Allen <ydcode@users.noreply.github.com>
2018-02-27 15:42:03 +0800

    Update B_Soft_Docker.sh

--- server_init/include/B_Soft_Docker.sh
+++ server_init/include/B_Soft_Docker.sh
@@ -19,7 +19,7 @@ Install_Docker_Fedora()
 Install_Docker_Ubuntu()
 {
         sudo apt-get update -y
-        sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+        sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
         sudo add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
         apt-get update -y
