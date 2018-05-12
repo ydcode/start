df459d3ea9322496606457d857b21704d9e495ca
Allen <ydcode@users.noreply.github.com>
2018-05-13 01:36:11 +0800

    Update B_Soft_Docker.sh

--- server_init/include/B_Soft_Docker.sh
+++ server_init/include/B_Soft_Docker.sh
@@ -40,7 +40,7 @@ Install_Docker_Debian()
 
 Install_Docker_Compose()
 {
-        sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+        sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
         docker-compose --version
 }
