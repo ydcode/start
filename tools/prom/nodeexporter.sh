c94ae64b86aa4ef00514c375bb6f64e56137cdb1
Allen <ydcode@users.noreply.github.com>
2018-05-21 14:29:37 +0800

    Update and rename tools/nodeexporter.sh to tools/prom/nodeexporter.sh

--- tools/prom/nodeexporter.sh
+++ tools/prom/nodeexporter.sh
@@ -1,3 +1,6 @@
+groupadd prometheus
+useradd -g prometheus -d /var/lib/prometheus -s /sbin/nologin prometheus
+
 wget https://github.com/prometheus/node_exporter/releases/download/v0.13.0/node_exporter-0.13.0.linux-amd64.tar.gz
 tar -zxvf node_exporter-0.13.0.linux-amd64.tar.gz
 mv node_exporter-0.13.0.linux-amd64 /usr/local/prometheus/node_exporter
