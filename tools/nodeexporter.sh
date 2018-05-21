dc30f6525e45d24cbac7a91b4bfc84d1cabe2884
Allen <ydcode@users.noreply.github.com>
2018-05-21 14:27:58 +0800

    Create nodeexporter.sh

---
+++ tools/nodeexporter.sh
@@ -0,0 +1,3 @@
+wget https://github.com/prometheus/node_exporter/releases/download/v0.13.0/node_exporter-0.13.0.linux-amd64.tar.gz
+tar -zxvf node_exporter-0.13.0.linux-amd64.tar.gz
+mv node_exporter-0.13.0.linux-amd64 /usr/local/prometheus/node_exporter
