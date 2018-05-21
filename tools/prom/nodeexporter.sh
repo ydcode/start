66d559f247d5b6d7a69d103de82c8e3905119f30
Allen <ydcode@users.noreply.github.com>
2018-05-21 14:38:25 +0800

    Update nodeexporter.sh

--- tools/prom/nodeexporter.sh
+++ tools/prom/nodeexporter.sh
@@ -5,6 +5,7 @@ wget https://github.com/prometheus/node_exporter/releases/download/v0.16.0/node_
 tar -zxvf node_exporter-0.16.0.linux-amd64.tar.gz
 mv node_exporter-0.16.0.linux-amd64 /usr/local/prometheus/node_exporter
 
+mkdir -p /usr/local/prometheus/node_exporte
 cp -f /root/start/tools/prom/node_exporter.service /usr/lib/systemd/system/node_exporter.service
 
 systemctl enable node_exporter
