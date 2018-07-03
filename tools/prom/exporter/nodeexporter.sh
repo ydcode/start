167388f419d446ae9188068e0b177f473baa50cf
Allen <ydcode@users.noreply.github.com>
2018-07-03 23:27:22 +0800

    Update nodeexporter.sh

--- tools/prom/exporter/nodeexporter.sh
+++ tools/prom/exporter/nodeexporter.sh
@@ -1,12 +1,12 @@
 groupadd prometheus
 useradd -g prometheus -d /var/lib/prometheus -s /sbin/nologin prometheus
 
-wget https://github.com/prometheus/node_exporter/releases/download/v0.16.0/node_exporter-0.16.0.linux-amd64.tar.gz
-tar -zxvf node_exporter-0.16.0.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v0.15.2/node_exporter-0.15.2.linux-amd64.tar.gz
+tar -zxvf node_exporter-0.15.2.linux-amd64.tar.gz
 
 mkdir -p /usr/local/prometheus/node_exporter
 
-mv node_exporter-0.16.0.linux-amd64/* /usr/local/prometheus/node_exporter/
+mv node_exporter-0.15.2.linux-amd64/* /usr/local/prometheus/node_exporter/
 
 cp -f /root/start/tools/prom/exporter/node_exporter.service /usr/lib/systemd/system/node_exporter.service
 
