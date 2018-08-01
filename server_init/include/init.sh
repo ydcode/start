9f9705657a0dcccf62c958b06950cc8303bd6f89
Allen <ydcode@users.noreply.github.com>
2018-08-01 12:53:38 +0800

    Update init.sh

--- server_init/include/init.sh
+++ server_init/include/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.tar.gz"
+JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz"
 MAVEN_URL="http://www-us.apache.org/dist/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz"
 
 JDK_FILE="jdk-8u172-linux-x64.tar.gz"
