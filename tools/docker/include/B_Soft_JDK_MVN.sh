8450a8134e9cf6fe79a1f0283d06a76d85c0c9d0
Allen <ydcode@users.noreply.github.com>
2020-06-02 17:33:16 +0800

    Update B_Soft_JDK_MVN.sh

--- tools/docker/include/B_Soft_JDK_MVN.sh
+++ tools/docker/include/B_Soft_JDK_MVN.sh
@@ -5,6 +5,7 @@ Install_JDK_DEBIAN()
 {
 # 	apt install -y openjdk-11-jdk
 	wget https://cdn.azul.com/zulu/bin/zulu14.28.21-ca-jdk14.0.1-linux_amd64.deb
+	apt --fix-broken install -y
 	dpkg -i zulu*.deb
 }
 
