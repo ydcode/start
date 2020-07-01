5d48d13bf93b9817136b8efbaf9e7ce31bcee9b4
Allen <ydcode@users.noreply.github.com>
2020-07-01 11:13:07 +0800

    Update B_Soft_Python.sh

--- tools/docker/include/B_Soft_Python.sh
+++ tools/docker/include/B_Soft_Python.sh
@@ -1,5 +1,9 @@
 Install_Docker_Debian()
 {
+        apt remove python
+        whereis python | xargs rm -rf
+        apt autoremove -y
+        
         sudo apt-get update -y
         sudo apt-get install -y zlib1g-dev
         wget https://www.python.org/ftp/python/3.7.8/Python-3.7.8.tar.xz
