7f970b60d4d777ed400af521e065e974c287217c
Allen <ydcode@users.noreply.github.com>
2020-07-16 10:51:31 +0800

    Create frp.sh

---
+++ tools/frp/frp.sh
@@ -0,0 +1,10 @@
+wget https://github.com/fatedier/frp/releases/download/v0.33.0/frp_0.33.0_linux_amd64.tar.gz
+tar -zxvf frp_0.33.0_linux_amd64.tar.gz --strip-components 1
+
+rm -rf frp_0.33.0_linux_amd64.tar.gz && rm -rf frpc*
+
+echo "" > frps.ini
+echo "[common]" >> frps.ini
+echo "bind_port = 7000" >> frps.ini
+echo "vhost_http_port = 80" >> frps.ini
+
