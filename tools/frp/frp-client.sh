0ffb443535f0879cebe9b67434a7f56862a4421c
Allen <ydcode@users.noreply.github.com>
2020-07-16 14:00:40 +0800

    Update frp-client.sh

--- tools/frp/frp-client.sh
+++ tools/frp/frp-client.sh
@@ -5,4 +5,4 @@ wget https://github.com/fatedier/frp/releases/download/v0.33.0/frp_0.33.0_linux_
 tar -zxvf frp_0.33.0_linux_amd64.tar.gz
 rm -rf frp_0.33.0_linux_amd64/frps*
 
-cp frp_0.33.0_linux_amd64/frpc /usr/bin/ && mkdir -p /etc/frp
+cp frp_0.33.0_linux_amd64/frpc /usr/bin/
