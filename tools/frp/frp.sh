1d1958f8ce063ad5205d6e0585e3037c66020997
Allen <ydcode@users.noreply.github.com>
2020-07-16 12:54:43 +0800

    Update frp.sh

--- tools/frp/frp.sh
+++ tools/frp/frp.sh
@@ -1,30 +1,15 @@
+apt install wget -y
 wget https://github.com/fatedier/frp/releases/download/v0.33.0/frp_0.33.0_linux_amd64.tar.gz
 tar -zxvf frp_0.33.0_linux_amd64.tar.gz --strip-components 1
 
 rm -rf frp_0.33.0_linux_amd64.tar.gz && rm -rf frpc*
 
+cp frps /usr/bin/
+
+mkdir -p /etc/frp
 
 cat > /etc/frp/frpc.ini<<EOF
 [common]
 bind_port = 7000
 vhost_http_port = 80
-EOF
-
-
-cat > /etc/systemd/system/frpc.service<<EOF
-[Unit]
-Description=frpc daemon
-After=syslog.target  network.target
-Wants=network.target
-
-[Service]
-Type=simple
-ExecStart=/usr/sbin/frp/frpc -c /etc/frp/frpc.ini
-Restart= always
-RestartSec=1min
-ExecStop=/usr/bin/killall frpc
-
-
-[Install]
-WantedBy=multi-user.target
 EOF
