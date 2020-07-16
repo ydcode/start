caeb9c3a0ff561cfecf375d3b8c149034df01e4d
Allen <ydcode@users.noreply.github.com>
2020-07-16 11:27:55 +0800

    Update frp.sh

--- tools/frp/frp.sh
+++ tools/frp/frp.sh
@@ -3,12 +3,28 @@ tar -zxvf frp_0.33.0_linux_amd64.tar.gz --strip-components 1
 
 rm -rf frp_0.33.0_linux_amd64.tar.gz && rm -rf frpc*
 
-echo "" > frps.ini
-echo "[common]" >> frps.ini
-echo "bind_port = 7000" >> frps.ini
-echo "vhost_http_port = 80" >> frps.ini
+
+cat > /etc/frp/frpc.ini<<EOF
+[common]
+bind_port = 7000
+vhost_http_port = 80
+EOF
 
 
-cp frps /usr/bin/
+cat > /etc/systemd/system/frpc.service<<EOF
+[Unit]
+Description=frpc daemon
+After=syslog.target  network.target
+Wants=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/sbin/frp/frpc -c /etc/frp/frpc.ini
+Restart= always
+RestartSec=1min
+ExecStop=/usr/bin/killall frpc
 
 
+[Install]
+WantedBy=multi-user.target
+EOF
