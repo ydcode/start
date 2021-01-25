d19f6bec4a7f91dc2ea94ad1b19dc3ec34eddf97
Allen <ydcode@users.noreply.github.com>
2021-01-26 02:33:26 +0800

    Update Debian_V2Ray.sh

--- tools/V2Ray/Debian_V2Ray.sh
+++ tools/V2Ray/Debian_V2Ray.sh
@@ -1,1 +1,21 @@
-##
+#!/bin/bash
+
+
+
+Install_V2Ray()
+{
+
+    Echo_Yellow "V2Ray Installing....."
+
+    sudo apt update
+    
+    bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+    systemctl enable v2ray
+    systemctl start v2ray
+
+
+}
+
+
+
+
