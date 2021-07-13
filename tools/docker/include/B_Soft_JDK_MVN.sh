4971c658693b696fe43ed4c68808466316e4cdc6
Allen <ydcode@users.noreply.github.com>
2021-07-14 01:54:41 +0800

    Update B_Soft_JDK_MVN.sh

--- tools/docker/include/B_Soft_JDK_MVN.sh
+++ tools/docker/include/B_Soft_JDK_MVN.sh
@@ -6,8 +6,8 @@ Install_JDK_DEBIAN()
 	#wget https://cdn.azul.com/zulu/bin/zulu14.29.23-ca-jdk14.0.2-linux_x64.tar.gz
 	#mkdir -p /usr/java && tar -xzvf zulu14.29.23-ca-jdk14.0.2-linux_x64.tar.gz --strip-components 1 -C /usr/java
 	
-	wget https://cdn.azul.com/zulu/bin/zulu15.28.51-ca-jdk15.0.1-linux_x64.tar.gz
-	mkdir -p /usr/java && tar -xzvf zulu15.28.51-ca-jdk15.0.1-linux_x64.tar.gz --strip-components 1 -C /usr/java
+	wget https://cdn.azul.com/zulu/bin/zulu16.30.15-ca-jdk16.0.1-linux_x64.tar.gz
+	mkdir -p /usr/java && tar -xzvf zulu16.30.15-ca-jdk16.0.1-linux_x64.tar.gz --strip-components 1 -C /usr/java
 
 }
 
