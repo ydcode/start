066a0254c22f389412966e02ee5e99e054279958
Allen <ydcode@users.noreply.github.com>
2022-04-09 11:34:04 +0800

    Update docker_container_clean.sh

--- tools/docker/docker_container_clean.sh
+++ tools/docker/docker_container_clean.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+chmod +x docker_container_clean*
 #*/3 * * * * /home/docker_container_clean.sh >> /var/log/docker_container_clean.log 2>&1
 
 CONTAINER_KEYWORD=${1:-'super_proxy__'}
@@ -7,10 +8,12 @@ MAX_CONTAINER_NUMBER=${2:-8}
 DELETE_CONTAINER_NUMBER=${3:-3}
 
 if [ $(docker ps | grep "${CONTAINER_KEYWORD}" | awk 'END{print FNR}') -gt "${MAX_CONTAINER_NUMBER}" ]; then
-  DOCKER_CONTAINER_LIST=$(docker ps | grep "${CONTAINER_KEYWORD}" | awk  '{print $10, $14}' | tail -"${DELETE_CONTAINER_NUMBER}" | awk '{print $2}')
-  echo "Container Count > ${MAX_CONTAINER_NUMBER}, will delete oldest ${DELETE_CONTAINER_NUMBER}"
+  DOCKER_CONTAINER_LIST=$(docker ps | grep "${CONTAINER_KEYWORD}" | awk  '{print $10, $14}' | tail -"${DELETE_CONTAINER_NUMBER}" | awk '{printf $2 " " }')
   echo "${DOCKER_CONTAINER_LIST}"
-  docker rm -f "${DOCKER_CONTAINER_LIST}"
+
+  #不要双引号
+  sudo docker rm -f ${DOCKER_CONTAINER_LIST}
+  echo "Container Count > ${MAX_CONTAINER_NUMBER}, will delete oldest ${DELETE_CONTAINER_NUMBER}"
 fi
 
 
