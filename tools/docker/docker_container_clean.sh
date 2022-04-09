715dda714732fa496ba20280048a535bbd1b277b
Allen <ydcode@users.noreply.github.com>
2022-04-09 10:52:29 +0800

    Create docker_container_clean.sh

---
+++ tools/docker/docker_container_clean.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#*/3 * * * * /home/docker_container_clean.sh >> /var/log/docker_container_clean.log 2>&1
+
+CONTAINER_KEYWORD=${1:-'super_proxy__'}
+MAX_CONTAINER_NUMBER=${2:-8}
+DELETE_CONTAINER_NUMBER=${3:-3}
+
+if [ $(docker ps | grep "${CONTAINER_KEYWORD}" | awk 'END{print FNR}') -gt "${MAX_CONTAINER_NUMBER}" ]; then
+  DOCKER_CONTAINER_LIST=$(docker ps | grep "${CONTAINER_KEYWORD}" | awk  '{print $10, $14}' | tail -"${DELETE_CONTAINER_NUMBER}" | awk '{print $2}')
+  echo "Container Count > ${MAX_CONTAINER_NUMBER}, will delete oldest ${DELETE_CONTAINER_NUMBER}"
+  echo "${DOCKER_CONTAINER_LIST}"
+  docker rm -f "${DOCKER_CONTAINER_LIST}"
+fi
+
+
+#docker rm -f $(docker ps | grep ${CONTAINER_KEYWORD} | grep -E '[0-9]{2,4} hours' | awk '{print $14}')
+#[ $(docker ps | grep super | grep -E '[0-9]{2,4} hours' | awk 'END{print FNR}') -gt 10 ] && echo 111 || echo 222
