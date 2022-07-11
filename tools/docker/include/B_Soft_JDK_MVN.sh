06ffda8c8c15b8481a1d47487c16899d8cae2155
Allen <ydcode@users.noreply.github.com>
2022-07-11 17:06:15 +0800

    Update B_Soft_JDK_MVN.sh

--- tools/docker/include/B_Soft_JDK_MVN.sh
+++ tools/docker/include/B_Soft_JDK_MVN.sh
@@ -18,8 +18,8 @@ Install_JDK_DEBIAN()
 
 Install_MVN_DEBIAN()
 {
-	MAVEN_URL="https://downloads.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz"
-	MAVEN_NAME="apache-maven-3.8.4"
+	MAVEN_URL="https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz"
+	MAVEN_NAME="apache-maven-3.8.6"
 	rm -rf /usr/maven
         if [ ! -d "/usr/maven/" ]; then
 		cd ${CurrentDir}
