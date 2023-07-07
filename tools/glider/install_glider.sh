6ad0bad37e70eb11459d739faa35908cfaa2b4c8
root <root@DEV>
2023-07-07 16:51:58 +0800

    add

--- tools/glider/install_glider.sh
+++ tools/glider/install_glider.sh
@@ -11,7 +11,7 @@ else
 fi
 
 # 安装必要的命令
-for cmd in jq wget curl; do
+for cmd in jq wget curl pgrep; do
     if ! [ -x "$(command -v $cmd)" ]; then
         sudo $PKG_MANAGER update -y
         sudo $PKG_MANAGER install $cmd -y
@@ -22,12 +22,16 @@ generate_random_string() {
     cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1
 }
 
-RANDOM_STRING=$(generate_random_string)
-
 GLIDER_VERSION="0.16.3"
 GLIDER_DIR="glider_${GLIDER_VERSION}_linux_amd64"
 GLIDER_FILE="${GLIDER_DIR}.tar.gz"
 GLIDER_URL="https://github.com/nadoo/glider/releases/download/v${GLIDER_VERSION}/${GLIDER_FILE}"
+CONFIG_FILE="./glider_config.json"
+
+# 如果没有找到配置文件，结束所有的 glider 进程
+if ! [ -f "${CONFIG_FILE}" ]; then
+    pkill -f glider
+fi
 
 # 检查并删除已存在的旧安装包
 if [ -f "${GLIDER_FILE}" ]; then
@@ -45,24 +49,44 @@ if ! [ -x "$(command -v ./glider)" ]; then
   cd ${GLIDER_DIR} || { echo "cd failed. Exiting."; exit 1; }
 fi
 
-#检查glider是否正在运行
-if ! pgrep -x "glider" > /dev/null
-then
-    #glider没有运行，开始运行
-    nohup ./glider -listen socks5://admin:${RANDOM_STRING}@:10888 >/dev/null 2>&1 &
-fi
-
 #获取本机公共IP
 PUBLIC_IP=$(curl -s https://checkip.amazonaws.com) || { echo "curl failed. Exiting."; exit 1; }
 
-# 输出分割符
-echo "=========================================="
+if [ -f "$CONFIG_FILE" ]; then
+    # Glider正在运行，读取配置文件并生成JSON
+    cat ${CONFIG_FILE} | jq || { echo "jq failed. Exiting."; exit 1; }
 
-#生成JSON输出
-echo -n "{\"type\":\"socks5\",\"IP\":\"${PUBLIC_IP}\",\"port\":10888,\"username\":\"admin\",\"password\":\"${RANDOM_STRING}\"}" | jq || { echo "jq failed. Exiting."; exit 1; }
+    if ! pgrep -x "glider" > /dev/null
+    then
+        echo "Glider is not running. Starting..."
+        PASSWORD=$(jq -r '.password' ${CONFIG_FILE})
+        nohup ./glider -listen socks5://admin:${PASSWORD}@:10888 >/dev/null 2>&1 &
+    fi
+else
+    # Glider没有运行，开始运行
+    RANDOM_STRING=$(generate_random_string)
+    nohup ./glider -listen socks5://admin:${RANDOM_STRING}@:10888 >/dev/null 2>&1 &
 
-# 输出分割符
-echo "=========================================="
+    #保存配置到文件
+    echo -n "{\"type\":\"socks5\",\"IP\":\"${PUBLIC_IP}\",\"port\":10888,\"username\":\"admin\",\"password\":\"${RANDOM_STRING}\"}" > ${CONFIG_FILE}
+
+    # 输出分割符
+    echo "========================================================================"
+
+    #生成JSON输出
+    cat ${CONFIG_FILE} | jq || { echo "jq failed. Exiting."; exit 1; }
+
+    # 输出分割符
+    echo "========================================================================"
+fi
+
+# 检查并输出 Glider 是否在运行
+if pgrep -x "glider" > /dev/null
+then
+    echo "Glider is running."
+else
+    echo "Glider is not running."
+fi
 
 # 删除旧的安装包
 rm -f ${GLIDER_FILE}
