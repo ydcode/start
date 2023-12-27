f89687cbba731b071756f61617a83cf92dddc185
Allen <ydcode@users.noreply.github.com>
2023-12-27 18:30:36 +0800

    Update restore.sh

--- tools/mysql/restore.sh
+++ tools/mysql/restore.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+docker rm -f mysql
+apt-get update && apt-get install -y wget curl sudo lsb-release && wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb && sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb && sudo percona-release setup ps80 && sudo apt-get update && sudo apt-get install -y percona-xtrabackup-80
+
+xtrabackup --prepare --target-dir=/home/temp_transfer/full_2023-12-27-05-00
+rm -rf /var/lib/docker/volumes/mysql_data/_data/*
+
+xtrabackup --copy-back --target-dir=/home/temp_transfer/full_2023-12-27-05-00 --datadir=/var/lib/docker/volumes/mysql_data/_data
