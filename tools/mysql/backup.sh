61d8a84e02ce290835628b4b842d850f1a5b81df
root <root@DEV>
2024-02-16 21:02:44 +0800

    add

--- tools/mysql/backup.sh
+++ tools/mysql/backup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+BACKUP_BASE_DIR=/home/backup/mysql
+FULL_BACKUP_PREFIX="full_"
+
 CONFIG_FILE="/tmp/backup_config.txt"
 REMOTE_DIR="/home/temp_transfer"
 
-FULL_BACKUP_PREFIX="full_"
-INCREMENTAL_BACKUP_PREFIX="incremental_"
 
 get_latest_full_backup() {
     local latest_backup=$(ls -1d $BACKUP_BASE_DIR/${FULL_BACKUP_PREFIX}* 2>/dev/null | sort -r | head -n 1)
@@ -44,39 +45,12 @@ setup_database_password() {
     fi
 }
 
-
-BACKUP_BASE_DIR=/home/backup/mysql
-
-echo "Select backup method:"
-echo "1) Full"
-echo "2) Incremental"
-read -p "Enter your choice (1 or 2): " BACKUP_METHOD
-BACKUP_METHOD=$(echo $BACKUP_METHOD | xargs)
-
 setup_database_password
 
 CURRENT_DATETIME=$(date +"%Y-%m-%d-%H-%M")
 
-case $BACKUP_METHOD in
-    1)
-        echo "Full backup selected."
-        BACKUP_DIR="${FULL_BACKUP_PREFIX}${CURRENT_DATETIME}"
-        ;;
-    2)
-        echo "Incremental backup selected."
-        LATEST_FULL_BACKUP_DIR=$(get_latest_full_backup)
+BACKUP_DIR="${FULL_BACKUP_PREFIX}${CURRENT_DATETIME}"
 
-        if [ $? -ne 0 ]; then
-            echo "No full backup found. Incremental backup requires a full backup. Aborting."
-            exit 1
-        fi
-        BACKUP_DIR="${INCREMENTAL_BACKUP_PREFIX}${CURRENT_DATETIME}"
-        ;;
-    *)
-        echo "Invalid selection."
-        exit 1
-        ;;
-esac
 
 mkdir -p $BACKUP_BASE_DIR/$BACKUP_DIR
 
@@ -84,12 +58,7 @@ XTRABACKUP_CMD="xtrabackup --backup --user=root --password=$DATABASE_PASSWORD --
 
 XTRABACKUP_CMD+=" --tables-exclude='command_control.data_ebay_search2' --tables-exclude='command_control.logs_command_control_request_log'"
 
-if [ "$BACKUP_METHOD" = "2" ]; then
-    XTRABACKUP_CMD="$XTRABACKUP_CMD --incremental-basedir=$LATEST_FULL_BACKUP_DIR"
-fi
-
-echo "The following command will be executed:"
-echo $XTRABACKUP_CMD
+echo "XTRABACKUP_CMD: $XTRABACKUP_CMD"
 
 read -p "Are you sure you want to proceed with the backup? (y/n): " CONFIRM_BACKUP
 CONFIRM_BACKUP=$(echo $CONFIRM_BACKUP | xargs)
@@ -98,6 +67,7 @@ if [ "$CONFIRM_BACKUP" = "y" ]; then
     docker exec -it mysql bash -c "apt-get update && apt-get install -y wget curl sudo lsb-release && wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb && sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb && sudo percona-release setup ps80 && sudo apt-get update && sudo apt-get install -y percona-xtrabackup-80"
     docker exec -it mysql bash -c "$XTRABACKUP_CMD"
 
+    docker exec -it mysql bash -c "ls -al"
     docker exec -it mysql bash -c "ls -al $BACKUP_BASE_DIR"
     docker exec -it mysql bash -c "ls -al $BACKUP_BASE_DIR/$BACKUP_DIR"
 
