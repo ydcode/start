752f7a3648f56b35efed997cfe20bc97130ce218
Allen <ydcode@users.noreply.github.com>
2024-06-15 12:55:50 +0800

    Update install.sh

--- tools/docker/install.sh
+++ tools/docker/install.sh
@@ -74,6 +74,15 @@ Install_JDK_DEBIAN() {
   source /etc/profile
 }
 
+Install_GRADLE() {
+  wget https://services.gradle.org/distributions/gradle-8.8-bin.zip
+  mkdir -p /usr/gradle && unzip -q gradle-8.8-bin.zip -d /usr/gradle && mv /usr/gradle/gradle-8.8/* /usr/gradle/
+  grep -q "export GRADLE_HOME=/usr/gradle" /etc/profile || echo "export GRADLE_HOME=/usr/gradle" >> /etc/profile
+  grep -q "\$GRADLE_HOME/bin" /etc/profile || echo "export PATH=\$GRADLE_HOME/bin:\$PATH" >> /etc/profile
+  source /etc/profile
+}
+
+
 Install_Docker_Debian() {
   curl -fsSL https://get.docker.com -o get-docker.sh
   sudo sh ./get-docker.sh
