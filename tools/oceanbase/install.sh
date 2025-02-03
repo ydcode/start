f85cb5ee07c36111f39a40e2f9120992fee4d8ef
root <root@DEV>
2025-02-03 22:51:38 +0800

    add

--- tools/oceanbase/install.sh
+++ tools/oceanbase/install.sh
@@ -10,9 +10,18 @@ print_green() {
   echo -e "\033[32m${text}\033[0m"
 }
 
-ENABLE_WLAN() {
-  ip link delete myvlan4000 2>/dev/null || true; ip link add link enp6s0 name myvlan4000 type vlan id 4000
-  ip link set myvlan4000 mtu 1400; ip link set myvlan4000 up; ip addr add 192.168.100.1/24 dev myvlan4000; ip addr show dev myvlan4000
+ENABLE_WLAN() {#Delete IPv6 First? /etc/network/interfaces
+#  ip link delete myvlan4000 2>/dev/null || true; ip link add link enp6s0 name myvlan4000 type vlan id 4000
+#  ip link set myvlan4000 mtu 1400; ip link set myvlan4000 up; ip addr add 192.168.100.1/24 dev myvlan4000; ip addr show dev myvlan4000
+sudo bash -c "cat > /etc/network/interfaces.d/vlan1000 << 'EOF'
+auto enp6s0.3600
+iface enp6s0.3600 inet static
+  address 192.168.100.1
+  netmask 255.255.255.0
+  vlan-raw-device enp6s0
+  mtu 1400
+EOF"
+
 }
 
 Install_ESSENTIAL__DEBIAN() {
