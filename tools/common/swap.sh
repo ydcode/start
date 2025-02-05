028559181c66817ff46af4aaef3147de18e07720
root <root@DEV>
2025-02-05 16:12:34 +0800

    add

--- tools/common/swap.sh
+++ tools/common/swap.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 disable_swap__debian() {
-  echo "Swap is enabled, shutting it down..."
   grep -q "vm.swappiness = 0" /etc/sysctl.conf || echo "vm.swappiness = 0" >> /etc/sysctl.conf
   swapoff -a && swapon -a
   sysctl -p
-  echo "Swap has been disabled and permanently turned off"
 }
 
 disable_swap() {
